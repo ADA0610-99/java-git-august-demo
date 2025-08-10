9432669a40d76f33e2877b0e78f1ab048a72c1f6
Madonna <dinara_93_mail@mail.ru>
2025-08-10 20:16:17 +0500

    мелкая доработка

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,10 +3,11 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Калькулятор начало");
+        System.out.println("Задача вычитания");
         int a = 10;
         int b = 10;
         int result = a + b;
-        System.out.println(result);
+        System.out.println(a + " - " + b + " = " + result);
 
     }
 }
