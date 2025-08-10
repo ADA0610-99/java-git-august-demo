572d8f17931b53e33f9f7d744d06e7f103a1612b
Madonna <dinara_93_mail@mail.ru>
2025-08-10 20:14:47 +0500

    решена задача вычитания

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,5 +3,10 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("Калькулятор начало");
+        int a = 10;
+        int b = 10;
+        int result = a + b;
+        System.out.println(result);
+
     }
 }
