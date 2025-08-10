4b77dc1f6d7d5846298972c9cafc56876faca9b6
Madonna <dinara_93_mail@mail.ru>
2025-08-10 20:10:02 +0500

    фикс пробелов

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -8,7 +8,7 @@ public class GitApp {
         int a = 1;
         int b = 2;
         int c = a+b;
-        System.out.println(a + "+" + b + "=" + c);
+        System.out.println(a + " + " + b + " = " + c);
 
     }
 }
