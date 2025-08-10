23de7db47a95eb0e8825964ad696453f7a1d0d8e
Madonna <dinara_93_mail@mail.ru>
2025-08-10 18:25:49 +0500

    научился работатьс переменными

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -5,6 +5,10 @@ public class GitApp {
         System.out.println(1);
         System.out.println("Hello world!");
         System.out.println(2);
+        int b=1;
+        int a=0;
+        int c = a+b;
+        System.out.println(c);
 
     }
 }
