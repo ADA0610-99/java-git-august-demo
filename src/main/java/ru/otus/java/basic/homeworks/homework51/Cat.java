68d6a96a4b1306172333d00485b60275aa457e17
Madonna <dinara_93_mail@mail.ru>
2025-09-10 22:47:17 +0500

    введение в ооп1 часть 2 (5.1)

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework51/Cat.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework51;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int runSpeed, int endurance) {
+        super(name, runSpeed, endurance);
+    }
+
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework51/Hors.java
@@ -0,0 +1,41 @@
+package ru.otus.java.basic.homeworks.homework51;
+
+
+import java.lang.module.FindException;
+
+public class Hors extends Animal {
+
+    private final int swimSpeed; //скорость плавания
+    private final int exhaustionRateForSwim; //скорость потери выносливости при плавании
+
+    public Hors(String name, int runSpeed, int endurance, int swimSpeed) {
+        super(name, runSpeed, endurance);
+        this.swimSpeed = swimSpeed;
+        this.exhaustionRateForSwim = 4;
+    }
+
+
+    public int swim(int distance) {
+        int time = distance / this.swimSpeed;
+        this.endurance = this.endurance - (distance * exhaustionRateForSwim);
+        if (this.endurance <= 0) {
+            System.out.println("У животного появилось состояние усталости");
+            return -1;
+        }
+        System.out.println("Животное проплыло:" + distance + " - за " + time + " секунд");
+        return time;
+    }
+
+
+    public void info() {
+        System.out.println(this.name +
+                " имеет единиц выносливости: " + this.endurance +
+                " при скорости бега: " + this.runSpeed +
+                " и скорости потери выносливости при беге: " + this.exhaustionRateForRun +
+                " при скорости плавания: " + this.swimSpeed +
+                " и скорости потери выносливости при плавании: " + this.exhaustionRateForSwim
+        );
+    }
+
+
+}
