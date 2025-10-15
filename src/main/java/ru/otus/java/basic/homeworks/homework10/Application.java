617fc6ec5bd36fc9f22279c32049a5509fd7634d
Madonna <dinara_93_mail@mail.ru>
2025-10-15 23:13:06 +0500

    разбор алгоритмов 10.1

--- src/main/java/ru/otus/java/basic/homeworks/homework10/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/Application.java
@@ -1,24 +1,28 @@
 package ru.otus.java.basic.homeworks.homework10;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         Map<Long, String> book = new HashMap<>();
 
-        PhoneBook book1 = new PhoneBook(book);
+        PhoneBook book1 = new PhoneBook();
 
-        book1.add(998919119143L, "Ivanov Ivan Ivanovich");
-        book1.add(998919119144L, "Ivanov Ivan Ivanovich");
-        book1.add(998911119111L, "Smirnova Mary Sergeevna");
-        book1.add(998911114566L, "Baranov Tom Borisovich");
-        book1.add(998911114566L, "Sidorov Petr Nikolaevich");
+        book1.add("Ivanov Ivan Ivanovich", 9989099999999L);
+        book1.add("Ivanov Ivan Ivanovich", 9989099999980L);
+        book1.add("Smirnova Mary Sergeevna", 9989099999970L);
+        book1.add("Baranov Tom Borisovich", 9989099999960L);
+        book1.add("Sidorov Petr Nikolaevich", 9989099999950L);
 
         System.out.println(book1.find("Ivanov Ivan Ivanovich"));
         System.out.println(book1.find("Sidorov Petr Nikolaevich"));
         System.out.println(book1.find("Semenov Ivan Nikolaevich"));
 
+
         System.out.println("Поиск владельца номера телефона: " + book1.containsPhoneNumber(998911119566L));
+        System.out.println("Поиск владельца номера телефона: " + book1.containsPhoneNumber(9989099999950L));
     }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework10/PhoneBook.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/PhoneBook.java
@@ -3,27 +3,33 @@ package ru.otus.java.basic.homeworks.homework10;
 import java.util.*;
 
 public class PhoneBook {
-    private Map<Long, String> book;
+    private Map<String, List> book;
 
-    public PhoneBook(Map<Long, String> book) {
-        this.book = book;
+    public PhoneBook() {
+        this.book = new HashMap();
     }
 
-    public void add(Long telephone, String fio) {
-        book.put(telephone, fio);
+    public void add(String fio, Long telephone) {
+        if (book.containsKey(fio)) {
+            book.get(fio).add(telephone);
+            return;
+        }
+        List<Long> telephones = new ArrayList<>();
+        telephones.add(telephone);
+        book.put(fio, telephones);
     }
 
     public List<Long> find(String fio) {
-        List<Long> phones = new ArrayList<>();
-        for (Map.Entry<Long, String> entry : book.entrySet()) {
-            if (entry.getValue() == fio) {
-                phones.add(entry.getKey());
-            }
-        }
-        return phones;
+        return book.get(fio);
     }
 
-    public String containsPhoneNumber(Long phone) {
-        return book.getOrDefault(phone, phone + " - номер телефона в справочнике отсутствует");
+    public boolean containsPhoneNumber(Long phone) {
+        for (Map.Entry<String, List> map : this.book.entrySet()) {
+            if (map.getValue().contains(phone)) {
+                return true;
+            }
+        }
+        return false;
     }
+
 }
